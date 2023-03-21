34a484d820c3d87fc32e313a86a4a9a035e8374a
Habbo420 <100413007+Habbo420@users.noreply.github.com>
2023-03-21 13:56:44 +0200

    Added Helper files

---
+++ src/main/java/za/ac/cput/util/SupplierHelper.java
@@ -0,0 +1,28 @@
+package za.ac.cput.util;
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.UUID;
+import java.util.regex.*;
+public class SupplierHelper {
+    public static boolean isNullOrEmpty(String str){
+        if (str == null || str.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidEmail(String email) {
+        EmailValidator ev = EmailValidator.getInstance();
+        return ev.isValid(email);
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber){
+        Pattern phoneNumberPattern = Pattern.compile("^\\d{10}$");
+        Matcher findAMatch = phoneNumberPattern.matcher(phoneNumber);
+        return (findAMatch.matches());
+    }
+
+    public static String generateId(){
+        return UUID.randomUUID().toString();
+    }
+}
